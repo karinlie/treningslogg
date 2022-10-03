283be9578877163e438d5f9befe3fab7b35528cf
karinlie <81482313+karinlie@users.noreply.github.com>
2022-10-03 08:55:03 +0200

    Add github deploy action

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,9 +7,16 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+	 // https://kit.svelte.dev/docs/adapters
+	 adapter: adapter({
+		// default options are shown. On some platforms
+		// these options are set automatically — see below
+		pages: 'build',
+		assets: 'build',
+		fallback: null,
+		precompress: false
+	  }),
+	  trailingSlash: 'always',
 };
 
 export default config;
